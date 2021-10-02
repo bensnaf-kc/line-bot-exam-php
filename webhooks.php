9ab5e041bfd5080d20d227e51c8b125f9ca24a53
bensnaf-kc <80310420+bensnaf-kc@users.noreply.github.com>
2021-10-02 23:14:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,51 +1,97 @@
-<?php // callback.php
-
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
-$access_token = 'Yc7epxagkTDtxlDZVmNicqE921hrLs3jn6fH/IWym3c1Wf7wHTuG7CfHoSuROOXiq0QGv37GiIHuMZvYVbAfcySjFifvh2kFd4/5azEHb1ZzyFvkFI6gQKR9JfBN1gdxwopvrIqeGf2hS1JD1BJ2eQdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
+<?php
+ $LINEData = file_get_contents('php://input');
+ $jsonData = json_decode($LINEData,true);
+ $replyToken = $jsonData["events"][0]["replyToken"];
+ $text = $jsonData["events"][0]["message"]["text"];
+ 
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "db_systemgarage";
+ $mysql = new mysqli($servername, $username, $password, $dbname);
+ mysqli_set_charset($mysql, "utf8");
+ 
+ if ($mysql->connect_error){
+ $errorcode = $mysql->connect_error;
+ print("MySQL(Connection)> ".$errorcode);
+ }
+ 
+ function sendMessage($replyJson, $token){
+   $ch = curl_init($token["URL"]);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+       'Content-Type: application/json',
+       'Authorization: Bearer ' . $token["AccessToken"])
+       );
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+   $result = curl_exec($ch);
+   curl_close($ch);
+return $result;
 }
-echo "OK";
+ 
+ $getText = $mysql->query("SELECT * FROM `fixcar` WHERE `f_tel`='$text' or f_line = '$text'");
+ $getuserNum = $getText->num_rows;
+ 
+ if ($getuserNum == "0"){
+     $message = '{
+     "type" : "text",
+     "text" : "ไม่มีข้อมูลที่ต้องการ"
+     }';
+     $replymessage = json_decode($message);
+ } else {
+  
+   while($row = mysqli_fetch_array($getText)){
+   	$Textcar = $row[0];
+   }
+   $sql_findCar = $mysql->query("SELECT * FROM `car` WHERE `id_fix`='$Textcar'");
+	 while($row_car = mysqli_fetch_array($getText)){
+	 	if($rowcar['type_idfix'] == 1 ){
+			$mess_car = '{
+			"type" : "text",
+			"text" : "รอดำเนินการ"
+			}';
+			$replymessage = json_decode($mess_car);
+		}
+		if($rowcar['type_idfix'] == 2 ){
+			$mess_car = '{
+			"type" : "text",
+			"text" : "กำลังซ่อม"
+			}';
+			$replymessage = json_decode($mess_car);
+		}
+		if($rowcar['type_idfix'] == 3 ){
+			$mess_car = '{
+			"type" : "text",
+			"text" : "รอการชำระ"
+			}';
+			$replymessage = json_decode($mess_car);
+		}
+		if($rowcar['type_idfix'] == 4 ){
+			$mess_car = '{
+			"type" : "text",
+			"text" : "ชำระเรียบร้อย"
+			}';
+			$replymessage = json_decode($mess_car);
+		}
+		if($rowcar['type_idfix'] == 5 ){
+			$mess_car = '{
+			"type" : "text",
+			"text" : "ระงับ"
+			}';
+			$replymessage = json_decode($mess_car);
+		}
+	 }
+ }
+ 
+ $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+ $lineData['AccessToken'] = "########";
+ $replyJson["replyToken"] = $replyToken;
+ $replyJson["messages"][0] = $replymessage;
+ 
+ $encodeJson = json_encode($replyJson);
+ 
+ $results = sendMessage($encodeJson,$lineData);
+ echo $results;
+ http_response_code(200);
